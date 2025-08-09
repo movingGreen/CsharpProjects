26aba1fa5d3fb7fff99a64132d30f595306a7b42
movingGreen <el_vailant@hotmail.com>
2025-08-09 08:49:27 -0400

    estudo matriz

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,19 +1,18 @@
-﻿/*
-string[] fraudulentOrderIds = new string[3];
+﻿string[] matriz =
+{  "B123"
+,"C234"
+,"A345"
+,"C15"
+,"B177"
+,"G3003"
+,"C235"
+, "B179"};
 
-fraudulentOrderIds[0] = "A123";
-fraudulentOrderIds[1] = "B456";
-fraudulentOrderIds[2] = "C789";
-// fraudulentOrderIds[4] = "D012";
-*/
-
-string[] fraudulentOrderIds = ["A123", "B456", "C789"];
-System.Console.WriteLine($"First: {fraudulentOrderIds[0]}");
-System.Console.WriteLine($"Second: {fraudulentOrderIds[1]}");
-System.Console.WriteLine($"Third: {fraudulentOrderIds[2]}");
-
-fraudulentOrderIds[0] = "F000";
-System.Console.WriteLine($"Reassign First: {fraudulentOrderIds[0]}");
-System.Console.WriteLine($"There are {fraudulentOrderIds.Length} fraudulent orders to process.");
-
+foreach (string item in matriz)
+{
+  if (item.StartsWith("B"))
+  {
+    Console.WriteLine(item);
+  }
+}
 
