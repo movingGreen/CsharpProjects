952b6e6498524b7598c61403fecc3cd52eb66949
movingGreen <el_vailant@hotmail.com>
2025-08-09 09:51:03 -0400

    estudo comentario

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,18 +1,23 @@
-﻿string[] matriz =
-{  "B123"
-,"C234"
-,"A345"
-,"C15"
-,"B177"
-,"G3003"
-,"C235"
-, "B179"};
+﻿/*
+  The following code create five random OrdersIDs
+  to test the fraud detection process. OrderIDs
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123
+*/
 
-foreach (string item in matriz)
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
 {
-  if (item.StartsWith("B"))
-  {
-    Console.WriteLine(item);
-  }
+  int prefixValue = random.Next(65, 70);
+  string prefix = Convert.ToChar(prefixValue).ToString();
+  string suffix = random.Next(1, 1000).ToString("000");
+  orderIDs[i] = prefix + suffix;
+}
+
+foreach (string orderID in orderIDs)
+{
+  Console.WriteLine(orderID);
 }
 
