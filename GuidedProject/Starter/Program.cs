f4275f94397a1d3b24f160c6a788219803901ed6
movingGreen <el_vailant@hotmail.com>
2025-08-11 15:19:02 -0400

    apresentando nota Sophia

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -8,20 +8,28 @@ int[] andrewScores = [92, 89, 81, 96, 90];
 int[] emmaScores = [90, 85, 87, 98, 68];
 int[] loganScores = [90, 95, 87, 88, 96];
 
-int sophiaSum = 0;
+string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
 
-decimal sophiaScore;
-
-foreach (int score in sophiaScores)
+foreach (string name in studentNames)
 {
-  sophiaSum += score;
+  if (name == "Sophia")
+  {
+    int sophiaSum = 0;
+
+    decimal sophiaScore;
+
+    foreach (int score in sophiaScores)
+    {
+      sophiaSum += score;
+    }
+
+    sophiaScore = (decimal)sophiaSum / currentAssignments;
+
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+  }
 }
 
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
