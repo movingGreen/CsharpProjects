55ec025c0c6683a919eda0743b2ecb999c332b8f
movingGreen <el_vailant@hotmail.com>
2025-08-11 15:48:35 -0400

    mostrando grades dos outros alunos

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -7,29 +7,46 @@ int[] sophiaScores = [90, 86, 87, 98, 100];
 int[] andrewScores = [92, 89, 81, 96, 90];
 int[] emmaScores = [90, 85, 87, 98, 68];
 int[] loganScores = [90, 95, 87, 88, 96];
+int[] studentScores = new int[10];
 
 string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
 
+Console.WriteLine("Student\t\tGrade\n");
+
 foreach (string name in studentNames)
 {
-  if (name == "Sophia")
+  string currentStudent = name;
+
+  if (currentStudent == "Sophia")
   {
-    int sophiaSum = 0;
-
-    decimal sophiaScore;
-
-    foreach (int score in sophiaScores)
-    {
-      sophiaSum += score;
-    }
-
-    sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-    Console.WriteLine("Student\t\tGrade\n");
-    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+    studentScores = sophiaScores;
   }
-}
+  else if (currentStudent == "Andrew")
+  {
+    studentScores = andrewScores;
+  }
+  else if (currentStudent == "Emma")
+  {
+    studentScores = emmaScores;
+  }
+  else if (currentStudent == "Logan")
+  {
+    studentScores = loganScores;
+  }
 
+  int sumAssignmentScores = 0;
+
+  decimal currentStudentGrade;
+
+  foreach (int score in studentScores)
+  {
+    sumAssignmentScores += score;
+  }
+
+  currentStudentGrade = (decimal)sumAssignmentScores / currentAssignments;
+
+  Console.WriteLine($"{currentStudent}:\t\t{currentStudentGrade}\t?");
+}
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
