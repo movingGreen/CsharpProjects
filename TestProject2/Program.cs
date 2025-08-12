e6b9a4989f82752c6d2c38a63222de0804ebd26f
movingGreen <el_vailant@hotmail.com>
2025-08-12 17:03:19 -0400

    operadores diversos

--- TestProject2/Program.cs
+++ TestProject2/Program.cs
@@ -1,22 +1,28 @@
-﻿// System.Console.WriteLine("a" == "a");
-// System.Console.WriteLine("a" == "A");
-// System.Console.WriteLine(1 == 2);
+﻿System.Console.WriteLine("a" == "a");
+System.Console.WriteLine("a" == "A");
+System.Console.WriteLine(1 == 2);
 
-// string myValue = "a";
-// System.Console.WriteLine(myValue == "a");
+string myValue = "a";
+System.Console.WriteLine(myValue == "a");
 
-// string value1 = " a";
-// string value2 = "A ";
-// System.Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+string value1 = " a";
+string value2 = "A ";
+System.Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
 
 System.Console.WriteLine("a" != "a");
 System.Console.WriteLine("a" != "A");
 System.Console.WriteLine(1 != 2);
 
-string myValue = "a";
-System.Console.WriteLine(myValue != "a");
+string myValue2 = "a";
+System.Console.WriteLine(myValue2 != "a");
 
 Console.WriteLine(1 > 2);
 Console.WriteLine(1 < 2);
 Console.WriteLine(1 >= 1);
 Console.WriteLine(1 <= 1);
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+System.Console.WriteLine(!pangram.Contains("fox"));
+System.Console.WriteLine(!pangram.Contains("cow"));
+System.Console.WriteLine(pangram.Contains("fox") == false);
+System.Console.WriteLine(!pangram.Contains("fox"));
