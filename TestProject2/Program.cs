3efb853052fd220a5379935afe5d773266d42edf
movingGreen <el_vailant@hotmail.com>
2025-08-12 16:34:25 -0400

    operadores diferentes que e maior e menor

--- TestProject2/Program.cs
+++ TestProject2/Program.cs
@@ -1,10 +1,22 @@
-﻿System.Console.WriteLine("a" == "a");
-System.Console.WriteLine("a" == "A");
-System.Console.WriteLine(1 == 2);
+﻿// System.Console.WriteLine("a" == "a");
+// System.Console.WriteLine("a" == "A");
+// System.Console.WriteLine(1 == 2);
+
+// string myValue = "a";
+// System.Console.WriteLine(myValue == "a");
+
+// string value1 = " a";
+// string value2 = "A ";
+// System.Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+System.Console.WriteLine("a" != "a");
+System.Console.WriteLine("a" != "A");
+System.Console.WriteLine(1 != 2);
 
 string myValue = "a";
-System.Console.WriteLine(myValue == "a");
+System.Console.WriteLine(myValue != "a");
 
-string value1 = " a";
-string value2 = "A ";
-System.Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
