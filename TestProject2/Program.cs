bdcd8ee65500f5e00222de1b66ea214f8761d941
movingGreen <el_vailant@hotmail.com>
2025-08-12 15:34:39 -0400

    início estudo expressões boolean

---
+++ TestProject2/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("a" == "a");
+System.Console.WriteLine("a" == "A");
+System.Console.WriteLine(1 == 2);
+
+string myValue = "a";
+System.Console.WriteLine(myValue == "a");
+
+string value1 = " a";
+string value2 = "A ";
+System.Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
