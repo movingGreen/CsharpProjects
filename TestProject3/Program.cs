66cef27e8cefc861069ac1842b26329fca7aaff5
movingGreen <el_vailant@hotmail.com>
2025-08-13 15:10:46 -0400

    teste permissões

--- TestProject3/Program.cs
+++ TestProject3/Program.cs
@@ -1,3 +1,29 @@
-﻿Random randomCoin = new Random();
-int coinValue = randomCoin.Next(0, 2);
-System.Console.WriteLine(coinValue == 0 ? "Heads" : "Tails");
+﻿string permission = "Manager";
+int level = 17;
+
+if (permission.Contains("Admin"))
+{
+  if (level > 55)
+  {
+    System.Console.WriteLine("Welcome, Super Admin user.");
+  }
+  else
+  {
+    System.Console.WriteLine("Welcome, Admin user.");
+  }
+}
+else if (permission.Contains("Manager"))
+{
+  if (level >= 20)
+  {
+    System.Console.WriteLine("Contact an Admin for access.");
+  }
+  else
+  {
+    System.Console.WriteLine("You do not have sufficient privileges.");
+  }
+}
+else
+{
+  System.Console.WriteLine("You do not have sufficient privileges.");
+}
