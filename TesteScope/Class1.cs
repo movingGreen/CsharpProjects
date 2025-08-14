9e6caf95217995987ba89ef79e0d746d2e419e3e
movingGreen <el_vailant@hotmail.com>
2025-08-14 15:11:42 -0400

    estudo block scope

---
+++ TesteScope/Class1.cs
@@ -0,0 +1,10 @@
+﻿bool flag = true;
+int value;
+
+if (true)
+{
+  value = 10;
+  System.Console.WriteLine($"Inside the code block: {value}");
+}
+
+System.Console.WriteLine($"Outside the code block: {value}");
