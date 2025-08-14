c9101794ee05a8f87d19f21ca82182505b7f1833
movingGreen <el_vailant@hotmail.com>
2025-08-14 16:12:00 -0400

    teste corrigir codigo bloco

--- TesteScope/Class1.cs
+++ TesteScope/Class1.cs
@@ -1,10 +1,17 @@
-﻿bool flag = true;
-int value;
+﻿int[] numbers = [4, 8, 15, 16, 23, 42];
+int total = 0;
+bool found = false;
 
-if (true)
+foreach (int number in numbers)
 {
-  value = 10;
-  System.Console.WriteLine($"Inside the code block: {value}");
+
+  total += number;
+
+  if (number == 42)
+    found = true;
 }
 
-System.Console.WriteLine($"Outside the code block: {value}");
+if (found)
+  Console.WriteLine("Set contains 42");
+
+System.Console.WriteLine($"Total: {total}");
