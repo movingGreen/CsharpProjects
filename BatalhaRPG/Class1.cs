bb0e1cb8e8a2f56baadafbe3590f6cd2ca6d34c2
movingGreen <el_vailant@hotmail.com>
2025-08-18 16:04:51 -0400

    batalha rpg com do while

---
+++ BatalhaRPG/Class1.cs
@@ -0,0 +1,30 @@
+﻿int vidaMonstro = 10;
+int vidaHeroi = 10;
+Random randomAtaque = new();
+
+
+do
+{
+  int heroiAtaque = randomAtaque.Next(1, 10);
+  vidaMonstro -= heroiAtaque;
+
+  System.Console.WriteLine($"Monster was damaged and lost {heroiAtaque} health and now has {vidaMonstro} health.");
+
+  if (vidaMonstro <= 0)
+  {
+    System.Console.WriteLine("Hero wins!");
+    break;
+  }
+
+  int monstroAtaque = randomAtaque.Next(1, 10);
+  vidaHeroi -= monstroAtaque;
+
+  System.Console.WriteLine($"Hero was damaged and lost {monstroAtaque} health and now has {vidaHeroi} health.");
+
+  if (vidaMonstro <= 0)
+  {
+    System.Console.WriteLine("Monster wins!");
+    break;
+  }
+
+} while (true);
