2cdf9bd70fabaeda7b1e5b2244e3043c5f8578d9
movingGreen <el_vailant@hotmail.com>
2025-08-18 15:37:50 -0400

    dowhile e while

--- DoWhile/Class1.cs
+++ DoWhile/Class1.cs
@@ -1,15 +1,20 @@
 ﻿Random random = new();
 int current = 4;
 
-// do
-// {
-//   current = random.Next(1, 10);
-//   Console.WriteLine(current);
-// } while (current != 7);
-
-while (current >= 3)
+do
 {
+  // current = random.Next(1, 10);
+  current++;
+
+  if (current >= 8) continue;
+
   Console.WriteLine(current);
-  current = random.Next(1, 11);
-}
-Console.WriteLine($"Last number: {current}");
+
+} while (current != 14);
+
+// while (current >= 3)
+// {
+//   Console.WriteLine(current);
+//   current = random.Next(1, 11);
+// }
+// Console.WriteLine($"Last number: {current}");
