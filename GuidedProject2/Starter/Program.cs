b5e82c3edec3ed598c9772966d381576f1ee7582
movingGreen <el_vailant@hotmail.com>
2025-08-20 17:33:29 -0400

    apresentando quantos pets

--- GuidedProject2/Starter/Program.cs
+++ GuidedProject2/Starter/Program.cs
@@ -130,7 +130,22 @@ do
 
         case "2":
             // Add a new animal friend to the ourAnimals array
-            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            string anotherPet = "y";
+            int petCount = 0;
+
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    petCount += 1;
+                }
+            }
+
+            if (petCount < maxPets)
+            {
+                Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {maxPets - petCount} more.");
+            }
+
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
