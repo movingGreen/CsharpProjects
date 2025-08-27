b61770dfd4519d19bb94d1c22a0a3b89052618a0
movingGreen <el_vailant@hotmail.com>
2025-08-27 16:03:24 -0400

    estudo tipo das variáveis

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Signed integral types:");
+
+System.Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
+System.Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
+System.Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
+System.Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+
+System.Console.WriteLine("");
+System.Console.WriteLine("Unsigned integral types:");
+
+System.Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+System.Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+System.Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+System.Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
