1507467547524be6a6f3b26bff5b9aa6158f4c4c
movingGreen <el_vailant@hotmail.com>
2025-09-04 10:48:44 -0400

    estudo array e metodos sort reverse clear resize

---
+++ ArrayMethods/Program.cs
@@ -0,0 +1,55 @@
+﻿string[] pallets = ["B14", "A11", "B12", "A13"];
+
+// System.Console.WriteLine("Sorted...");
+// Array.Sort(pallets);
+
+// foreach (var pallet in pallets)
+// {
+//   System.Console.WriteLine($"-- {pallet}");
+// }
+
+// System.Console.WriteLine("");
+// System.Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+
+// foreach (var pallet in pallets)
+// {
+//   System.Console.WriteLine($"-- {pallet}");
+// }
+
+System.Console.WriteLine($"Before: {pallets[0].ToLower()}");
+Array.Clear(pallets, 0, 2);
+
+if (pallets[0] != null)
+  System.Console.WriteLine($"After: {pallets[0].ToLower()}");
+
+
+System.Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+  System.Console.WriteLine($"-- {pallet}");
+}
+
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+  Console.WriteLine($"-- {pallet}");
+}
+
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+  Console.WriteLine($"-- {pallet}");
+}
