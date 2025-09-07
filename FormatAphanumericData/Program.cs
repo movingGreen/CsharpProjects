3c056cb8be2a7dbc1809e1230ae6b161fc4379e4
movingGreen <el_vailant@hotmail.com>
2025-09-06 22:33:31 -0400

    formatando alphanumericos

---
+++ FormatAphanumericData/Program.cs
@@ -0,0 +1,11 @@
+﻿string first = "Hello";
+string second = "World";
+
+System.Console.WriteLine($"{first} {second}!");
+System.Console.WriteLine($"{second} {first}!");
+System.Console.WriteLine($"{first} {first} {first}!");
+
+decimal price = 123.45m;
+int discount = 50;
+
+System.Console.WriteLine($"Price: {price:c} (Save {discount:c})");
