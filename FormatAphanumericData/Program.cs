0f242210a88732264626643605266db887e47e77
movingGreen <el_vailant@hotmail.com>
2025-09-09 17:05:06 -0400

    formating padleft e padright

--- FormatAphanumericData/Program.cs
+++ FormatAphanumericData/Program.cs
@@ -42,3 +42,19 @@
 
 // System.Console.WriteLine(result);
 
+string input = "Pad this";
+
+System.Console.WriteLine(input.PadLeft(12));
+System.Console.WriteLine(input.PadLeft(12, '-'));
+System.Console.WriteLine(input.PadRight(12, '-'));
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+System.Console.WriteLine("1234567890123456789012345678901234567890");
+System.Console.WriteLine(formattedLine);
