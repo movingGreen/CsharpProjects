cd0816edc7dacbf24c7e94500b4549a36792defc
movingGreen <el_vailant@hotmail.com>
2025-09-09 15:34:11 -0400

    formating multiple types

--- FormatAphanumericData/Program.cs
+++ FormatAphanumericData/Program.cs
@@ -17,9 +17,28 @@
 // decimal tax = .36785m;
 // System.Console.WriteLine($"Tax rate: {tax:p3}");
 
-decimal price = 67.55m;
-decimal salePrice = 59.99m;
-string yourDiscount = string.Format("You saved {0:c2} off the regular {1:c2} price. ", price - salePrice, price);
-yourDiscount += $"A discount of {((price - salePrice) / price):p0}!";
+// decimal price = 67.55m;
+// decimal salePrice = 59.99m;
+// string yourDiscount = string.Format("You saved {0:c2} off the regular {1:c2} price. ", price - salePrice, price);
+// yourDiscount += $"A discount of {((price - salePrice) / price):p0}!";
 
-System.Console.WriteLine(yourDiscount);
+// System.Console.WriteLine(yourDiscount);
+
+// int invoiceNumber = 1201;
+// decimal productShares = 25.4568m;
+// decimal subtotal = 2750.00m;
+// decimal taxPercentage = .15825m;
+// decimal total = 3185.19m;
+
+// System.Console.WriteLine($"Invoice Number: {invoiceNumber}");
+// System.Console.WriteLine($"   Shares: {productShares:N3} Product");
+// System.Console.WriteLine($"       Sub Total: {subtotal:C}");
+// System.Console.WriteLine($"           Tax: {taxPercentage:p2}");
+// System.Console.WriteLine($"       Total Billed: {total:c}");
+
+// string first = "Hello";
+// string second = "World";
+// string result = string.Format("{0} {1}!", first, second);
+
+// System.Console.WriteLine(result);
+
