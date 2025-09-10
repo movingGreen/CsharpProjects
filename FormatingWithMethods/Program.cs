e423cdf7b4a8f3e8b49189ca63a0939631678389
movingGreen <el_vailant@hotmail.com>
2025-09-10 09:45:10 -0400

    modificar string com métodos

---
+++ FormatingWithMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿// string message = "Find what is (inside the parentheses)";
+
+// int openingPosition = message.IndexOf('(');
+// int closingPosition = message.IndexOf(')');
+// openingPosition += 1;
+
+// // System.Console.WriteLine(openingPosition);
+// // System.Console.WriteLine(closingPosition);
+
+// int length = closingPosition - openingPosition;
+
+// System.Console.WriteLine(message.Substring(openingPosition, length));
+
+// string message = "What is the value <span>between the tags</span>?";
+// const string openSpan = "<span>";
+// const string closeSpan = "</span>";
+
+// int openingPosition = message.IndexOf(openSpan);
+// int closingPosition = message.IndexOf(openSpan);
+// openingPosition += 6;
+
+// // System.Console.WriteLine(openingPosition);
+// // System.Console.WriteLine(closingPosition);
+
+// int length = closingPosition - openingPosition;
+
+// System.Console.WriteLine(message.Substring(openingPosition, length));
+
