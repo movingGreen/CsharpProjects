413ed791e515261e5d5dd53101b91f7a03e05b68
Movinggreen <el_vailant@hotmail.com>
2025-09-11 22:25:02 -0400

    usando replace

--- FormatingWithMethods/Program.cs
+++ FormatingWithMethods/Program.cs
@@ -100,6 +100,12 @@
 // }
 
 string data = "12345John Smith          5000  3  ";
-string updateData = data.Remove(1, 20);
+string updateData = data.Remove(5, 20);
 
 Console.WriteLine(updateData);
+
+string message = "This--is--ex-amp-le--da-ta";
+message = message.Replace("--", " ");
+message = message.Replace("-", "");
+
+System.Console.WriteLine(message);
