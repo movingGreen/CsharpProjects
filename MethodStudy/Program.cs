6ee38279280a1bdf806ebcdaab9694fb997ebba3
movingGreen <el_vailant@hotmail.com>
2025-09-16 17:04:54 -0400

    montando avaliação de ipvalidos

--- MethodStudy/Program.cs
+++ MethodStudy/Program.cs
@@ -81,33 +81,36 @@ then ipAddress is valid
 else ipAddress is invalid
 */
 
-string ipv4Input = "107.31.1.5";
+string[] ipv4Input = ["107.31.1.5", "255.0.0.255", "555..0.555", "255...255"];
+string[] address;
 bool validLength = false;
 bool validZeroes = false;
 bool validRange = false;
 
-ValidateLength();
-ValidateZeroes();
-ValidateRange();
+foreach (string ip in ipv4Input)
+{
+  address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
+  ValidateLength();
+  ValidateZeroes();
+  ValidateRange();
 
-if (validLength && validZeroes && validRange)
-{
-  System.Console.WriteLine($"ip is a valid IPv4 address");
-}
-else
-{
-  System.Console.WriteLine($"ip is an invalid IPv4 address");
+  if (validLength && validZeroes && validRange)
+  {
+    System.Console.WriteLine($"{ip} is a valid IPv4 address");
+  }
+  else
+  {
+    System.Console.WriteLine($"{ip} is an invalid IPv4 address");
+  }
 }
 
 void ValidateLength()
 {
-  string[] address = ipv4Input.Split(".");
   validLength = address.Length == 4;
 }
 
 void ValidateZeroes()
 {
-  string[] address = ipv4Input.Split(".");
 
   foreach (string number in address)
   {
@@ -123,7 +126,6 @@ void ValidateZeroes()
 
 void ValidateRange()
 {
-  string[] address = ipv4Input.Split(".", StringSplitOptions.RemoveEmptyEntries);
 
   foreach (string number in address)
   {
