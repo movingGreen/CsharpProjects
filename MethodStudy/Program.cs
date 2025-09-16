16906e88570525fb8b9e5b83d01de87fc876ca4f
movingGreen <el_vailant@hotmail.com>
2025-09-16 14:19:30 -0400

    código pseudocódigo

--- MethodStudy/Program.cs
+++ MethodStudy/Program.cs
@@ -1,71 +1,82 @@
-﻿using System;
+﻿// using System;
 
-int[] times = { 800, 1200, 1600, 2000 };
-int diff = 0;
+// int[] times = { 800, 1200, 1600, 2000 };
+// int diff = 0;
 
-Console.WriteLine("Enter current GMT");
-int currentGMT = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter current GMT");
+// int currentGMT = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Current Medicine Schedule:");
+// Console.WriteLine("Current Medicine Schedule:");
 
-DisplayTimes();
+// DisplayTimes();
 
-Console.WriteLine("Enter new GMT");
-int newGMT = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter new GMT");
+// int newGMT = Convert.ToInt32(Console.ReadLine());
 
-if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
-{
-  Console.WriteLine("Invalid GMT");
-}
-else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
-{
-  diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+// if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
+// {
+//   Console.WriteLine("Invalid GMT");
+// }
+// else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0)
+// {
+//   diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
 
-  AdjustTimes();
-}
-else
-{
-  diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+//   AdjustTimes();
+// }
+// else
+// {
+//   diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
 
-  AdjustTimes();
-}
+//   AdjustTimes();
+// }
 
-Console.WriteLine("New Medicine Schedule:");
+// Console.WriteLine("New Medicine Schedule:");
 
-DisplayTimes();
+// DisplayTimes();
 
-void DisplayTimes()
-{
-  /* Format and display medicine times */
-  foreach (int val in times)
-  {
-    string time = val.ToString();
-    int len = time.Length;
+// void DisplayTimes()
+// {
+//   /* Format and display medicine times */
+//   foreach (int val in times)
+//   {
+//     string time = val.ToString();
+//     int len = time.Length;
 
-    if (len >= 3)
-    {
-      time = time.Insert(len - 2, ":");
-    }
-    else if (len == 2)
-    {
-      time = time.Insert(0, "0:");
-    }
-    else
-    {
-      time = time.Insert(0, "0:0");
-    }
+//     if (len >= 3)
+//     {
+//       time = time.Insert(len - 2, ":");
+//     }
+//     else if (len == 2)
+//     {
+//       time = time.Insert(0, "0:");
+//     }
+//     else
+//     {
+//       time = time.Insert(0, "0:0");
+//     }
 
-    Console.Write($"{time} ");
-  }
+//     Console.Write($"{time} ");
+//   }
 
-  Console.WriteLine();
-}
+//   Console.WriteLine();
+// }
 
-void AdjustTimes()
-{
-  /* Adjust the times by adding the difference, keeping the value within 24 hours */
-  for (int i = 0; i < times.Length; i++)
-  {
-    times[i] = ((times[i] + diff)) % 2400;
-  }
-}
+// void AdjustTimes()
+// {
+//   /* Adjust the times by adding the difference, keeping the value within 24 hours */
+//   for (int i = 0; i < times.Length; i++)
+//   {
+//     times[i] = ((times[i] + diff)) % 2400;
+//   }
+// }
+
+/*
+if ipAddress consists of 4 numbers
+and 
+if each ipAddress number has no leading zeroes
+and if each ipAddress number is in range 0 - 255
+
+then ipAddress is valid 
+
+else ipAddress is invalid
+*/
