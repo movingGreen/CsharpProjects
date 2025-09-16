0ee6d5416b51290c5705b354e21d1f60d2b8754e
movingGreen <el_vailant@hotmail.com>
2025-09-16 14:59:30 -0400

    method validateLength and validateZeroes

--- MethodStudy/Program.cs
+++ MethodStudy/Program.cs
@@ -80,3 +80,44 @@ then ipAddress is valid
 
 else ipAddress is invalid
 */
+
+string ipv4Input = "107.31.1.5";
+bool validLength = false;
+bool validZeroes = false;
+bool validRange = false;
+
+ValidateLength();
+ValidateZeroes();
+ValidateRange();
+
+if (validLength && validZeroes && validRange)
+{
+  System.Console.WriteLine($"ip is a valid IPv4 address");
+}
+else
+{
+  System.Console.WriteLine($"ip is an invalid IPv4 address");
+}
+
+void ValidateLength()
+{
+  string[] address = ipv4Input.Split(".");
+  validLength = address.Length == 4;
+}
+
+void ValidateZeroes()
+{
+  string[] address = ipv4Input.Split(".");
+
+  foreach (string number in address)
+  {
+    if (number.Length > 1 && number.StartsWith("0"))
+    {
+      validZeroes = false;
+    }
+  }
+
+  validZeroes = true;
+}
+
+void ValidateRange() { }
