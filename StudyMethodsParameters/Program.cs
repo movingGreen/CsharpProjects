9e48c682823426a259f18efd5517d311ebab985e
movingGreen <el_vailant@hotmail.com>
2025-09-18 17:23:21 -0400

    parametros nomeados

--- StudyMethodsParameters/Program.cs
+++ StudyMethodsParameters/Program.cs
@@ -113,14 +113,63 @@
 
 // ============================================
 
-string status = "Healthy";
+// string status = "Healthy";
 
-System.Console.WriteLine($"Start: {status}");
-SetHealth(false);
-System.Console.WriteLine($"End: {status}");
+// System.Console.WriteLine($"Start: {status}");
+// SetHealth(false);
+// System.Console.WriteLine($"End: {status}");
 
-void SetHealth(bool isHealthy)
+// void SetHealth(bool isHealthy)
+// {
+//   status = (isHealthy ? "Healthy" : "Unhealthy");
+//   System.Console.WriteLine($"Middle: {status}");
+// }
+// ===============================
+
+string[] guestList = ["Rebecca", "Nadia", "Noor", "Jonte"];
+string[] rsvps = new string[10];
+int count = 0;
+
+RSVP("Rebecca", 1, "none", true);
+RSVP("Nadia", 2, "Nuts", true);
+RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
+RSVP("Tony", 1, "Jackfruit", true);
+RSVP("Noor", 4, "none", false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+
+void RSVP(string name, int partySize, string allergies, bool inviteOnly)
 {
-  status = (isHealthy ? "Healthy" : "Unhealthy");
-  System.Console.WriteLine($"Middle: {status}");
+  if (inviteOnly)
+  {
+    bool found = false;
+
+    foreach (string guest in guestList)
+    {
+      if (guest.Equals(name))
+      {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found)
+    {
+      System.Console.WriteLine($"Sorry, {name} is not on the guest list");
+      return;
+    }
+  }
+
+  rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+  count++;
+}
+
+void ShowRSVPs()
+{
+  System.Console.WriteLine("\nTotal RSVPs:");
+
+  for (int i = 0; i < count; i++)
+  {
+    System.Console.WriteLine(rsvps[i]);
+  }
 }
