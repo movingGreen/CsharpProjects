d8bae2bc3b52013bafa6c4c51e218c56ad8a7641
movingGreen <el_vailant@hotmail.com>
2025-09-18 15:34:16 -0400

    exemplo estudo variáveis string dependendo do escopo

--- StudyMethodsParameters/Program.cs
+++ StudyMethodsParameters/Program.cs
@@ -74,15 +74,53 @@
 // }
 // ===========================================
 
-int a = 3;
-int b = 4;
-int c = 0;
+// int a = 3;
+// int b = 4;
+// int c = 0;
 
-Multiply(a, b, c);
-Console.WriteLine($"global statement: {a} x {b} = {c}");
+// Multiply(a, b, c);
+// Console.WriteLine($"global statement: {a} x {b} = {c}");
 
-void Multiply(int a, int b, int c)
+// void Multiply(int a, int b, int c)
+// {
+//   c = a * b;
+//   System.Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+// }
+// ========================================
+// int[] array = [1, 2, 3, 4, 5];
+
+// PrintArray(array);
+// Clear(array);
+// PrintArray(array);
+
+// void PrintArray(int[] array)
+// {
+//   foreach (int a in array)
+//   {
+//     Console.Write($"{a} ");
+//   }
+
+//   System.Console.WriteLine();
+// }
+
+// void Clear(int[] array)
+// {
+//   for (int i = 0; i < array.Length; i++)
+//   {
+//     array[i] = 0;
+//   }
+// }
+
+// ============================================
+
+string status = "Healthy";
+
+System.Console.WriteLine($"Start: {status}");
+SetHealth(false);
+System.Console.WriteLine($"End: {status}");
+
+void SetHealth(bool isHealthy)
 {
-  c = a * b;
-  System.Console.WriteLine($"inside Multiply method: {a} x {b} = {c}");
+  status = (isHealthy ? "Healthy" : "Unhealthy");
+  System.Console.WriteLine($"Middle: {status}");
 }
