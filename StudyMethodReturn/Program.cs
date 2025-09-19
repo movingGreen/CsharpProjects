a13d2e4e06bae7209bb1cb24250884d6deddbcf6
movingGreen <el_vailant@hotmail.com>
2025-09-19 17:32:44 -0400

    separando código

--- StudyMethodReturn/Program.cs
+++ StudyMethodReturn/Program.cs
@@ -114,51 +114,54 @@
 
 // ====================================
 
-int target = 80;
-int[] coins = [5, 5, 50, 25, 25, 10, 5];
-int[,] result = TwoCoins(coins, target);
+// int target = 80;
+// int[] coins = [5, 5, 50, 25, 25, 10, 5];
+// int[,] result = TwoCoins(coins, target);
 
-if (result.Length == 0)
-{
-  System.Console.WriteLine("No two coins make change");
-}
-else
-{
-  System.Console.WriteLine("Change found at positions:");
+// if (result.Length == 0)
+// {
+//   System.Console.WriteLine("No two coins make change");
+// }
+// else
+// {
+//   System.Console.WriteLine("Change found at positions:");
 
-  for (int i = 0; i < result.GetLength(0); i++)
-  {
-    if (result[i, 0] == -1)
-    {
-      break;
-    }
-    System.Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
-  }
-}
+//   for (int i = 0; i < result.GetLength(0); i++)
+//   {
+//     if (result[i, 0] == -1)
+//     {
+//       break;
+//     }
+//     System.Console.WriteLine($"{result[i, 0]},{result[i, 1]}");
+//   }
+// }
 
-int[,] TwoCoins(int[] coins, int target)
-{
-  int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
-  int count = 0;
+// int[,] TwoCoins(int[] coins, int target)
+// {
+//   int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+//   int count = 0;
 
-  for (int curr = 0; curr < coins.Length; curr++)
-  {
-    for (int next = curr + 1; next < coins.Length; next++)
-    {
-      if (coins[curr] + coins[next] == target)
-      {
-        result[count, 0] = curr;
-        result[count, 1] = next;
-        count++;
-      }
+//   for (int curr = 0; curr < coins.Length; curr++)
+//   {
+//     for (int next = curr + 1; next < coins.Length; next++)
+//     {
+//       if (coins[curr] + coins[next] == target)
+//       {
+//         result[count, 0] = curr;
+//         result[count, 1] = next;
+//         count++;
+//       }
 
-      if (count == result.GetLength(0))
-        return result;
-    }
-  }
+//       if (count == result.GetLength(0))
+//         return result;
+//     }
+//   }
 
-  if (count == 0)
-    return new int[0, 0];
+//   if (count == 0)
+//     return new int[0, 0];
 
-  return result;
-}
+//   return result;
+// }
+
+//================================================
+
