b2531526b1813879b0d527b1326c37e014bd638e
movingGreen <el_vailant@hotmail.com>
2025-09-19 10:17:06 -0400

    exemplo trabalhando com retorno de métodos

--- StudyMethodReturn/Program.cs
+++ StudyMethodReturn/Program.cs
@@ -1,10 +1,21 @@
 ﻿double total = 0;
-double minimumSpend = 30.00;
+double minimumSpend = 1000.00;
 
 double[] items = [15.97, 3.50, 12.25, 22.99, 10.98];
 double[] discounts = [0.30, 0.00, 0.10, 0.20, 0.50];
 
-Console.WriteLine($"Total: ${total}");
+for (int i = 0; i < items.Length; i++)
+{
+  total += GetDiscountedPrice(i);
+}
+
+if (TotalMeetsMinimum())
+{
+  total -= 5.00;
+}
+
+Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
 
 double GetDiscountedPrice(int itemIndex)
 {
@@ -12,13 +23,15 @@ double GetDiscountedPrice(int itemIndex)
   return items[itemIndex] * (1 - discounts[itemIndex]); ;
 }
 
-void TotalMeetsMinimum()
+bool TotalMeetsMinimum()
 {
   // Check if the total meets the minimum
+  return total >= minimumSpend;
 }
 
-void FormatDecimal(double input)
+string FormatDecimal(double input)
 {
   // Format the double so only 2 decimal places are displayed
+  return input.ToString()[..5];
 }
 
