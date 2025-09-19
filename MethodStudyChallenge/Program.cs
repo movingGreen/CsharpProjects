19016015b2803d26d5b78f0604226999e4b204d4
Movinggreen <el_vailant@hotmail.com>
2025-09-18 21:58:40 -0400

    challenge estudo método com parâmetros

---
+++ MethodStudyChallenge/Program.cs
@@ -0,0 +1,35 @@
+﻿string[,] corporate =
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+  // display internal email addresses
+  ShowEmail(firstName: corporate[i, 0], lastName: corporate[i, 1], domain: externalDomain);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+  // display external email addresses
+  ShowEmail(firstName: external[i, 0], lastName: external[i, 1], domain: externalDomain);
+
+}
+
+void ShowEmail(string firstName, string lastName, string domain)
+{
+  firstName = firstName.ToLower().AsSpan(0, 2).ToString();
+  lastName = lastName.ToLower();
+
+  System.Console.WriteLine($"{firstName}{lastName}@{domain}");
+}
