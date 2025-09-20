fb67665e2459a68f5b73a99369b1ffdb1cc69541
movingGreen <el_vailant@hotmail.com>
2025-09-20 10:26:11 -0400

    estudo uso de métodos teste jogo de dados

--- StudyMethodReturn/Program.cs
+++ StudyMethodReturn/Program.cs
@@ -165,3 +165,48 @@
 
 //================================================
 
+Random random = new();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+
+if (ShouldPlay())
+{
+  PlayGame();
+}
+
+void PlayGame()
+{
+  var play = true;
+
+  while (play)
+  {
+    var target = random.Next(1, 6);
+    var roll = random.Next(1, 7);
+
+    Console.WriteLine($"Roll a number greater than {target} to win!");
+    Console.WriteLine($"You rolled a {roll}");
+    Console.WriteLine(WinOrLose(roll, target));
+    Console.WriteLine("\nPlay again? (Y/N)");
+
+    play = ShouldPlay();
+  }
+}
+
+bool ShouldPlay()
+{
+  string? input = Console.ReadLine();
+
+  if (!string.IsNullOrWhiteSpace(input))
+  {
+    input = input.ToUpper();
+
+    return input == "Y";
+  }
+
+  return false;
+}
+
+string WinOrLose(int roll, int target)
+{
+  return roll > target ? "You win!" : "You lose!";
+}
